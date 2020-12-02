99223acf4cc0aecd6717e8372a40d25e55d74ddb
Penroll <39489247+Penroll@users.noreply.github.com>
2020-12-02 10:52:37 -0500

    Add files via upload

--- AdventOfCode2020Day1.java
+++ AdventOfCode2020Day1.java
@@ -1,46 +1,24 @@
 package Day1;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class AdventOfCode2020Day1 {
 
     public static boolean isMultiplied(int one, int two, int three){
-        if(one + two + three == 2020){
-            return true;
-        }
-        else {
-            return false;
-        }
+        return one + two + three == 2020;
     }
-
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scnr = new Scanner(System.in);
-        Scanner input = getInput(scnr);
+        Scanner input = new Scanner(new File("/Users/srgrealey/IdeaProjects/AdventOfCode2020/src/Day1/DayOneInput"));
         int value = -1;
         int[] values = getValues(input);
-        for(int i = 0; i < values.length; i++){
-            for(int j = 0; j < values.length; j++){
-                for(int k = 0; k < values.length; k++){
-                    if(isMultiplied(values[i],values[j],values[k])){
-                        value = values[i]*values[j]*values[k];
-                    }
-                }
-
-
-            }
-        }
+        for (int i : values)
+            for (int j : values)
+                for (int k : values)
+                    if (isMultiplied(i, j, k)) value = i * j * k;
         System.out.println(value);
     }
-
-    public static Scanner getInput(Scanner scnr) throws FileNotFoundException {
-        Scanner input = null;
-        input = new Scanner(new File("/Users/srgrealey/IdeaProjects/AdventOfCode2020/src/Day1/DayOneInput"));
-        return input;
-    }
-
     public static int[] getValues(Scanner input){
         int[] values = new int[200];
         int i =0;
